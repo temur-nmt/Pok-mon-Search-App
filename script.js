2af9c537dcfff5def83aec642fd4f851ad778b8f
temur-nmt <temur.nmt@gmail.com>
2025-03-09 10:48:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+const searchBtn = document.getElementById("search-button");
+const userInput = document.getElementById("search-input");
+const pokemonApi = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
+
+const getPokemon = async () => {
+    try {
+        const res = await fetch(pokemonApi);
+        const data = await res.json();
+        const {results} = data;
+        const pokemonToSearch = userInput.value.toLowerCase();
+        console.log(results); 
+        
+        let allPokemonNames = [];
+        results.forEach(({id,name,url}) => {
+            allPokemonNames.push(name);
+        });
+        console.log(allPokemonNames);
+        console.log(results);
+
+        if (!allPokemonNames.includes(pokemonToSearch)) {
+            alert("Pokémon not found");
+        } else {
+            const pokemonUrl = results[allPokemonNames.indexOf(pokemonToSearch)].url;
+            console.log(pokemonToSearch);
+            console.log(pokemonUrl);
+            fetch(pokemonUrl)
+                .then((res) => res.json())
+                .then((data) => {
+                    updateStats(data);
+                })
+                .catch((err) => console.log(`Data Error: ${err}`));
+        }
+    } catch (err) {
+        console.log(`Caught Error: ${err}`);
+    }
+};
+
+const updateStats = (data) => {
+    console.log(data);
+    const {base_experience, height,id,name,order,sprites,stats,types,weight} = data;
+    const {base_stat, effort, stat} = stats;
+    console.log("Stats:", stats);
+    
+    const pName = document.getElementById("pokemon-name");
+    const pId = document.getElementById("pokemon-id");
+    const pWeight = document.getElementById("weight");
+    const pHeight = document.getElementById("height");
+
+
+    const hp = document.getElementById("hp");
+    const attack = document.getElementById("attack");
+    const defense = document.getElementById("defense");
+    const spAttack = document.getElementById("special-attack");
+    const spDefense = document.getElementById("special-defense");
+    const speed = document.getElementById("speed");
+    const pTypes = document.getElementById("types");
+
+    pName.textContent = name.toUpperCase();
+    pId.textContent = id;
+    pWeight.textContent = weight;
+    pHeight.textContent = height;
+
+    pTypes.innerHTML = "";
+    console.log(`These are the types:`, types);
+    types.forEach((el) => {
+        const span = document.createElement("span");
+        span.textContent = el.type.name.toUpperCase();
+        pTypes.appendChild(span);
+    });
+
+    hp.textContent = stats[0].base_stat;
+    attack.textContent = stats[1].base_stat;
+    defense.textContent = stats[2].base_stat;
+    spAttack.textContent = stats[3].base_stat;
+    spDefense.textContent = stats[4].base_stat;
+    speed.textContent = stats[5].base_stat;
+
+};
+
+searchBtn.addEventListener("click", getPokemon);
